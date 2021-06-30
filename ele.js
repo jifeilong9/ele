c225fc651591dff19b72116d679952523232b6ea
67 <38131602+67here@users.noreply.github.com>
2021-07-01 00:38:06 +0800

    Add files via upload

--- ele.js
+++ ele.js
@@ -1,12 +1,8 @@
-
 //初始化
 var dev_hight = device.height;
 var dev_width = device.width;
 if(dev_hight && dev_width == 0) {toastLog('BUG啦，请重启手机');exit();}
 console.show();
-
-//2.点击不可点击的控件
-function position_click(x){if(x) click(x.bounds().centerX(), x.bounds().centerY());else toastLog('无法中心点击');}
 
 //子线程 音量键关闭
 threads.start(function(){
@@ -17,57 +13,53 @@ events.onKeyDown("volume_up", function(event){toastLog("\n音量+被按下，即
 //检测并点击
 function check_click() {
   text('下单任务').waitFor();
-
   sleep(2000);
-  let list_15sec = textContains('15秒').find();
-  sleep(500);
-  let list_0sec = textContains('浏览|10万').find();
-
-  for(i = 0; i < list_15sec.length; i++){
-    if(list_15sec[i] && list_15sec[i].parent().child(2).text() != '奖励已到账') {       
-      list_15sec[i].parent().child(2).click();
-      console.log(list_15sec[i].parent().child(0).getText())
-      console.log('等待...\n');
-      textMatches("任务完成|点击返回").waitFor();
-      back();
-      sleep(3000);
-    }    
-  }
-  
-  for(i = 0; i < list_0sec.length; i++){
-    if(list_0sec[i] && list_0sec[i].parent().child(2).text() != '奖励已到账') {
-      list_0sec[i].parent().child(2).click();
-    	sleep(3000);
-    	back();
-    	sleep(3000);
-    }
+  outer: 
+  while(1){
+		let list_15sec = textContains('15秒').find();
+    let list_0sec = textContains('浏览').find();
+    for(i = 0; i < list_15sec.length; i++){
+      if(list_15sec[i] && list_15sec[i].parent().child(2).text() != '奖励已到账') {
+        sleep(1000);
+        list_15sec[i].parent().child(2).click();
+        console.log(list_15sec[i].parent().child(0).getText())
+        console.log('等待...');
+        textMatches("任务完成|点击返回").waitFor();
+        back();sleep(500);
+        if(text('晚点再来').findOne(2000)) text('晚点再来').click();
+        sleep(1000);
+        text('逛逛任务').waitFor();
+        sleep(1000);
+        continue outer;
+      }
+    }  
+      
+    for(i = 0; i < list_0sec.length; i++){
+      if(list_0sec[i] && list_0sec[i].parent().child(2).text() != '奖励已到账') {
+          list_0sec[i].parent().child(2).click();
+          sleep(3000);
+          back();
+          if(text('晚点再来').findOne(2000)) text('晚点再来').click();
+          sleep(1000);
+        	text('逛逛任务').waitFor();
+        	sleep(1000);
+        	continue outer;
+      }
+  	} 
+    break;
   }
   toastLog('任务1 基本完成');
 }
 
 function video() {
-  text('下单任务').waitFor();
-  sleep(1000);
-  back();
-  toastLog('等待回到主界面中');
-  text('真香').waitFor();
-  toastLog('回到主界面');
-  sleep(1000);
-  position_click(text('真香').findOne());
-  text('推荐').waitFor();
-  sleep(2000);
-  click(dev_width / 2, dev_hight / 2);
-  toastLog('等待进入视频页内');
-  text('分享').waitFor();
-  toastLog('进入视频页 \n 视频会自动循环 刷完会自动退回桌面');
-  text('明日再领').waitFor();
+  textContains('说两句').waitFor();
+  toastLog('\n已进入视频页\n刷完 自动退回桌面');
+  text('明日再领').waitFor();  
   home();
   toastLog('已经刷完 自动结束')
 }
 
-
 //启动并跳转
-
 toastLog('启动饿了么\n按下音量+ 脚本停止');
 home();sleep(2000);
 app.startActivity({
@@ -75,6 +67,9 @@ app.startActivity({
   data: 'eleme://web?&url=https://h5.ele.me/svip/task-list'
   });
 
+//主程序
 check_click();
+log('请双击点进视频页中...\n 它在"订单"左边 \n等待中...')
 video();
+exit();
 
